234e3606ba90bd24b6905b35befd4ed22282df4a
stojnovsky <stojnovsky@gmail.com>
2016-12-16 12:19:53 +0200

    first

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * JORO SERVICE FILESYSTEM WATCHER - WINDOWS
+ * crematory SERVICE FILESYSTEM WATCHER - WINDOWS
  */
 
 
@@ -10,9 +10,14 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const express = require('express');
 const PKG = require('./package.json')
+const mkdirp = require('mkdirp');
+const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+var config = {};
 
 /**
  * [missing_config description]
@@ -20,37 +25,64 @@ const PKG = require('./package.json')
  * @param  {[type]}       msg [description]
  * @return {[type]}           [description]
  */
-const missing_config = function(msg){
-    console.log(msg);
+const exit = function(msg){
+    console.log('[err]', msg);
     process.exit(true);
 }
 
 try{
-    var config = PKG.joro_config
+    config = PKG.crematory_service || exit('Please add "crematory_service" section in package.json');
 }catch(e){
-    missing_config('Please add "joro_service" section in package.json');
+    exit('Please add "crematory_service" section in package.json');
 }
 
-
 config.home_dir = path.join(os.homedir())
-config.target = path.join(os.homedir(), config.target) || missing_config('Please add "target" for "joro_service" in package.json');
+config.target = path.join(os.homedir(), config.target) || exit('Please add "target" for "crematory_service" in package.json');
+config.port = config.port || '3000';
+config.host = config.host || '127.0.0.1'
+
+
+console.log('[info]','target location:', config.target);
 
 if (os.type().toLowerCase() != 'Windows'.toLowerCase() ){
     console.log('[warrning]','Service is write for Windows! Maybe not work correctly here');
 }
 
 fs.access(config.target, fs.constants.R_OK, function(err){
-
+    if(err) {
+        mkdirp(config.target, function (err) {
+            if (err) exit('No access to target location.')
+            else console.log('[Info]','target folder initialize.')
+            init()
+        });
+    }else{
+        init()
+    }
 })
 
 
 
-var app = express();
+
+function init(){
 
 
+    app.get('/', function(req, res){
+      res.sendFile(path.join(path.parse(process.mainModule.filename).dir, '/view/index.html'));
+    });
 
-fs.watch(config.target, {
-    recursive:true
-}, function(eventType, filename){
-    console.log('[ON]', eventType, filename);
-})
+    io.on('connection', function(socket){
+      console.log('a client connected');
+    });
+
+    http.listen(33333,'127.0.0.1', function(){
+
+      console.log('[Info]','listening on *:33333');
+
+      fs.watch(config.target, {
+        //   recursive:true
+      }, function(eventType, filename){
+          console.log('[ON]', eventType, filename);
+          io.emit('file_change', { for: 'everyone', eventType:eventType, filename:filename });
+      })
+    });
+}
